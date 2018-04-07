c75b898f3b4f38b2e5ba529a7e33d8b5c31e013c
AimeeLing <95793348@qq.com>
2018-04-07 18:41:11 +0800

    "默认配置生成验证码"

--- security-demo/src/main/java/org/google/code/DemoImageCodeGenerator.java
+++ security-demo/src/main/java/org/google/code/DemoImageCodeGenerator.java
@@ -2,13 +2,12 @@ package org.google.code;
 
 import org.google.security.core.validate.code.ImageCode;
 import org.google.security.core.validate.code.ValidateCodeGenerator;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.ServletWebRequest;
 
 /**
  * Created by wbcaoa on 2018/4/3.
  */
-@Component("imageCodeGenerator")
+//@Component("imageCodeGenerator")
 public class DemoImageCodeGenerator implements ValidateCodeGenerator {
 
 
